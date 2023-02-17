106ef331bbc56f97f333b0cddfd6d21ce36b0e99
DoguD <dogudeniz.ugur@gmail.com>
2023-02-17 23:43:02 +0800

    Initial commit

---
+++ src/main/java/Library.java
@@ -0,0 +1,9 @@
+public class Library {
+    public static void main(String arg[]) {
+        System.out.println("Hello, World!");
+    }
+
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
